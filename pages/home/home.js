f588aa50d546336a3caeebe2c2f79509552e159d
ck1049 <18238489029@163.com>
2023-07-13 21:51:25 +0800

    主页先用背景图片代替

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,7 +7,11 @@ Page({
     data: {
         
     },
-
+    goToLottery() {
+        wx.navigateTo({
+          url: '/pages/lottery-information/lottery-information',
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
